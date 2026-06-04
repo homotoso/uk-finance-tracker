aaf191911bcbc65469888766f849e41ba8a86e8c
habeeb <habeeb.omotoso1@gmail.com>
2026-06-04 23:32:42 +0100

    Set up Vite app entry files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}", "./finance-tracker.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
